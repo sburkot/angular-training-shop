b246bb93317675f93e5889a5ead8ab7930474b19
sburkot <32367871+sburkot@users.noreply.github.com>
2017-11-22 16:02:06 +0200

    Task 2 correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { ProductsService } from './services/products-service/products.service'
 import { CartService } from './services/cart-service/cart.service'
 
-import { Product } from './model/product.model'
-import { DataRepositoryService } from './services/data-repository/data-repository.service'
-import { ProductComponent } from './components/product/product.component'
-import { ProductListComponent} from './components/product-list/product-list.component'
-import { CartComponent } from './components/cart/cart.component'
-import { CartService } from './services/cart-service/cart.service'
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/services/products-service/products.service.ts
+++ src/app/services/products-service/products.service.ts
@@ -15,7 +15,7 @@ export class ProductsService {
       new Product(4, 'ZenPad', Category.TabletPC, 'Asus ZenPad 8.0 16GB', false, 170),
       new Product(5, 'Lenovo Tab', Category.TabletPC, 'Lenovo Tab 3-850 8" WiFi 16GB', true, 150),
       new Product(6, 'iPad', Category.TabletPC, 'Apple iPad Air 64g', true, 800, ['Galaxy Tab S3']),
-      new Product(7, 'Galaxy Tab', Category.TabletPC, 'Samsung Galaxy Tab S3 9.7" 32GB LTE', true, 750, ['Apple iPad Air 64g']),
+      new Product(7, 'Galaxy Tab', Category.TabletPC, 'Samsung Galaxy Tab S3 9.7" 32GB LTE', false, 750, ['Apple iPad Air 64g']),
       new Product(8, 'MacBook', Category.NoteBook, 'Apple A1466 MacBook Air 13', true, 1000)
     ];
   }
